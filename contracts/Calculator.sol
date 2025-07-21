7da7890c3f6be357b245cb453760a726ca0b14d0
Choseolmin <daegok1000@gmail.com>
2025-07-21 17:03:20 +0900

    1.0

--- contracts/Calculator.sol
+++ contracts/Calculator.sol
@@ -1,1 +1,20 @@
-contract Calculator {}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./AbstractCalculator.sol";
+
+contract Calculator is AbstractCalculator {
+    function calculate(uint256 a, uint256 b, string memory operation) public pure returns (uint256) {
+        if (keccak256(bytes(operation)) == keccak256(bytes("add"))) {
+            return add(a, b);
+        } else if (keccak256(bytes(operation)) == keccak256(bytes("subtract"))) {
+            return subtract(a, b);
+        } else if (keccak256(bytes(operation)) == keccak256(bytes("multiply"))) {
+            return multiply(a, b);
+        } else if (keccak256(bytes(operation)) == keccak256(bytes("divide"))) {
+            return divide(a, b);
+        } else {
+            revert("Invalid operation");
+        }
+    }
+}
